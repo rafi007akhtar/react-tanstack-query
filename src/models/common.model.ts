8b8e8b0861b3fbc6b465059d4c3ae37cf6bcd18d
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-02-25 20:54:18 +0530

    added new models for EventInfo and EventInfoWrapper

--- src/models/common.model.ts
+++ src/models/common.model.ts
@@ -1,8 +1,20 @@
+interface ErrorInfo {
+  message: string;
+}
+
+export interface EventInfo {
+  id: string;
+  date: string | Date;
+  image: string;
+  title: string;
+  location: string;
+}
+
+export interface EventInfoWrapper {
+  event: EventInfo;
+}
+
 export interface ResError extends Error {
   code?: number;
   info?: ErrorInfo;
-}
-
-interface ErrorInfo {
-  message: string;
 }
