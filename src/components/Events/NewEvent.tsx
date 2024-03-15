7a24638b2bda2e04abb154c4be5e02d0be1a3424
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-03-16 02:12:09 +0530

    using useMutation for new events and invalidating previous cached events data

--- src/components/Events/NewEvent.tsx
+++ src/components/Events/NewEvent.tsx
@@ -1,25 +1,51 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent, queryClient } from "../../util/http.util.js";
+import { NewEventData, ResError } from "../../models/common.model.js";
+import ErrorBlock from "../UI/ErrorBlock.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent, // unlike useQuery, mutationFn is not called automatically; you need to call it separately,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"], // invalidates all query with this key as a part of it
+        // exact: true,  // if written, it will invalidate the exact matching query, not all
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleSubmit(formData: NewEventData) {
+    mutate({ event: formData }); // object format is set in backend app.js file
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting ..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Something went wrong"
+          message={(error as ResError).info?.message}
+        />
+      )}
     </Modal>
   );
 }
